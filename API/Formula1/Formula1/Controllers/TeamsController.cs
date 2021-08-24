398d600cfc0065f9b00a7a7aa04d51cdd2a38cb2
reiniboy15 <65732914+reiniboy5@users.noreply.github.com>
2021-08-24 20:23:57 +0200

    Team Repo and Controller

---
+++ API/Formula1/Formula1/Controllers/TeamsController.cs
@@ -0,0 +1,97 @@
+﻿using Formula1.Entities;
+using Formula1.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Formula1.Controllers
+{
+    [Route("api/Teams")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        private readonly IAppRepository<Teams> _appRepository;
+
+        public TeamsController(IAppRepository<Teams> appRepository)
+        {
+            _appRepository = appRepository;
+        }
+
+        // GET: api/Teams
+        [HttpGet]
+        public IActionResult Get()
+        {
+            IEnumerable<Teams> teams = _appRepository.GetAll();
+
+            return Ok(teams);
+        }
+
+        // GET: api/Teams/{id}
+
+        [HttpGet("{id}", Name = "GetTeams")]
+        public IActionResult Get(long id)
+        {
+            Teams team = _appRepository.Get(id);
+
+
+            if (team == null)
+            {
+                return NotFound("Requested Team does not exist.");
+            }
+
+            return Ok(team);
+        }
+
+        // GET: api/Teams
+        [HttpPost]
+        public IActionResult Post([FromBody] Teams team)
+        {
+            if (team == null)
+            {
+                return BadRequest("Team is null.");
+            }
+            _appRepository.Add(team);
+            return CreatedAtRoute(
+                  "GetTeams",
+                  new { Id = team.TeamID },
+                  team);
+        }
+
+        // PUT: api/Teams/{5}
+        [HttpPut("{id}")]
+        public IActionResult Put(long id, [FromBody] Teams team)
+        {
+            if (team == null)
+            {
+                return BadRequest("Team is null.");
+            }
+
+            Teams teamToUpdate = _appRepository.Get(id);
+            if (teamToUpdate == null)
+            {
+                return NotFound("The Team does not exist.");
+            }
+            _appRepository.Update(teamToUpdate, team);
+
+            return NoContent();
+        }
+
+
+        // DELETE: api/Teams/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(long id)
+        {
+            Teams team = _appRepository.Get(id);
+            if (team == null)
+            {
+                return NotFound("The Team does not exist.");
+            }
+            _appRepository.Delete(team);
+
+            return NoContent();
+        }
+    }
+}
---
+++ API/Formula1/Formula1/Repository/TeamsRepository.cs
@@ -0,0 +1,55 @@
+﻿using Formula1.Data;
+using Formula1.Entities;
+using Formula1.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Formula1.Repository
+{
+    public class TeamsRepository : IAppRepository<Teams>
+    {
+        readonly Formula1DbContext formula1Db;
+
+
+        public TeamsRepository(Formula1DbContext _formula1Db)
+        {
+            formula1Db = _formula1Db;
+        }
+        public void Add(Teams teams)
+        {
+            formula1Db.Teams.Add(teams);
+            formula1Db.SaveChanges();
+        }
+
+        public void Delete(Teams teams)
+        {
+            formula1Db.Teams.Remove(teams);
+            formula1Db.SaveChanges();
+        }
+
+        public Teams Get(long id)
+        {
+            return formula1Db.Teams.FirstOrDefault(x => x.TeamID == id);
+        }
+
+        public IEnumerable<Teams> GetAll()
+        {
+            return formula1Db.Teams.ToList();
+        }
+
+        public void Update(Teams teams, Teams entity)
+        {
+            teams.TeamName = entity.TeamName;
+            teams.TeamBase = entity.TeamBase;
+            teams.TeamChief = entity.TeamChief;
+            teams.TeamChassis = entity.TeamChassis;
+            teams.TeamPowerUnit = entity.TeamPowerUnit;
+            teams.TeamWorldChampionships = entity.TeamWorldChampionships;
+            teams.TeamPolePositions = entity.TeamPolePositions;
+            teams.TeamFastestLaps = entity.TeamFastestLaps;
+            formula1Db.SaveChanges();
+        }
+    }
+}
