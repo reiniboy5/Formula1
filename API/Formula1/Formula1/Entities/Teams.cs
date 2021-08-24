7794285722c2c47a4ddbce6bf836e27e178330c2
reiniboy15 <65732914+reiniboy5@users.noreply.github.com>
2021-08-24 19:50:45 +0200

    Create Team Entity

---
+++ API/Formula1/Formula1/Entities/Teams.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Formula1.Entities
+{
+    public class Teams
+    {
+        [Key]
+        public int TeamID { get; set; }
+        public string TeamName { get; set; }
+        public string TeamBase { get; set; }
+        public string TeamChief { get; set; }
+        public string TeamChassis { get; set; }
+        public string TeamPowerUnit { get; set; }
+        public int TeamWorldChampionships { get; set; }
+        public int TeamPolePositions { get; set; }
+        public int TeamFastestLaps { get; set; }
+    }
+}
