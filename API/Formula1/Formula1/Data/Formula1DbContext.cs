f8905d8490030ce2e1a495e2070ddd9f0754285e
reiniboy15 <65732914+reiniboy5@users.noreply.github.com>
2021-08-24 19:44:26 +0200

    Add DbContext

---
+++ API/Formula1/Formula1/Data/Formula1DbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Formula1.Data
+{
+    public class Formula1DbContext : DbContext
+    {
+        public Formula1DbContext(DbContextOptions<Formula1DbContext> options) : base(options)
+        {
+
+        }
+    }
+}
